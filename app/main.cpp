020c67067e60ab7dd16b27dec2df239767b7db6c
ShaneFlandermeyer <shaneflandermeyer@gmail.com>
2021-11-30 20:55:09 -0600

    Sorta refactored stft function to use eigen. Still need to account for real vs complex fft values somewhere

--- app/main.cpp
+++ app/main.cpp
@@ -11,31 +11,31 @@
 
 using namespace matplot;
 int main() {
-  auto bandwidth = 10e6;
-  auto pulsewidth = 100e-6;
-  auto prf = {10e3};
-  auto sampRate = 20e6;
-  auto wave = LinearFMWaveform(bandwidth, pulsewidth, prf, sampRate);
-  auto x = wave.step();
-  // auto dt = 0.001;
-  // auto f0 = 50;
-  // auto f1 = 250;
-  // auto t1 = 2;
-  // auto x = std::vector<double>();
-  // for (double t = 0; t < t1; t += dt) {
-  //   x.push_back(
-  //       cos(2 * M_PI * t * (f0 + (f1 - f0) * pow(t, 2) / (3 * pow(t1, 2)))));
-  // }
+  // auto bandwidth = 10e6;
+  // auto pulsewidth = 100e-6;
+  // auto prf = {10e3};
+  // auto sampRate = 20e6;
+  // auto wave = LinearFMWaveform(bandwidth, pulsewidth, prf, sampRate);
+  // auto x = wave.step();
+  auto dt = 0.001;
+  auto f0 = 50;
+  auto f1 = 250;
+  auto t1 = 2;
+  auto x = std::vector<double>();
+  for (double t = 0; t < t1; t += dt) {
+    x.push_back(
+        cos(2 * M_PI * t * (f0 + (f1 - f0) * pow(t, 2) / (3 * pow(t1, 2)))));
+  }
   // plot(x);
   // show();
 
   // Short time fourier transform equation
-  auto noverlap = 64;
-  auto nfft = 256;
+  auto noverlap = 120;
+  auto nfft = 128;
   auto win = window::hamming(nfft);
-  auto spectro = spectrogram(x, win, nfft, noverlap);
-  image(spectro);
-  colorbar();
-  show();
+  auto spectro = stft(x, win, nfft, noverlap);
+  // image(spectro);
+  // colorbar();
+  // show();
   return 0;
 }
--- include/spectrogram.h
+++ include/spectrogram.h
@@ -4,45 +4,74 @@
 #include <fftw3.h>
 #include <matplot/matplot.h>
 
+#include <Eigen/Dense>
 #include <complex>
 #include <iostream>
+#include <unsupported/Eigen/FFT>
 #include <vector>
 
 #include "utils.h"
 
+using namespace Eigen;
 /**
  * @brief Short-time Fourier transform (STFT)
  *
  * @tparam T Input type
  * @param x Input signal
- * @param fs Sample Rate
  * @param window Spectral window
  * @param nfft Number of DFT points
  * @param noverlap Number of overlapped samples
  */
 template <typename T>
-std::vector<std::vector<std::complex<double>>> stft(std::vector<T> x,
-                                                    std::vector<double> window,
-                                                    int nfft, int noverlap) {
-  // Hop size
-  auto hopSize = window.size() - noverlap;
-  // Number of columns in the STFT matrix
-  auto k = (int)floor((x.size() - noverlap) / hopSize);
+Matrix<std::complex<double>, Dynamic, Dynamic> stft(std::vector<T> x, std::vector<T> window,
+                                 const int nFFT = 512,
+                                 const int nOverlap = 256) {
+  FFT<T> fft;
+  // fft.SetFlag(FFT<T>::HalfSpectrum);
+  auto hopSize = window.size() - nOverlap;
+  auto nCol = (int)floor((x.size() - nOverlap) / hopSize);
   // STFT matrix
-  auto X = std::vector<std::vector<std::complex<double>>>(
-      k, std::vector<std::complex<double>>(nfft));
-  // One window length of time series data
-  auto xi = std::vector<T>(window.size());
-  for (auto iStart = 0, iVec = 0; iStart < x.size() - window.size();
-       iStart += hopSize, iVec++) {
-    // Apply the window
-    std::transform(x.begin() + iStart, x.begin() + iStart + window.size(),
-                   window.begin(), xi.begin(), std::multiplies<T>());
-    // Compute the FFT
-    X[iVec] = fft(xi);
+  Matrix<std::complex<double>, Dynamic, Dynamic> stft(nFFT, nCol);
+  Matrix<T, Dynamic, 1> xi;
+  Matrix<T, Dynamic, 1> win =
+      Map<Matrix<T, Dynamic, 1>>(window.data(), window.size());
+  Matrix<std::complex<double>, Dynamic, 1> tmpOut;
+  for (auto iCol = 0; iCol < nCol; iCol++) {
+    // Multiply the current segment by the window
+    xi = Map<Matrix<T, Dynamic, 1>>(x.data() + iCol * hopSize, window.size());
+    xi = xi.array() * win.array();
+    // std::cout << xi << std::endl;
+    // Compute the FFT of the windowed segment
+    fft.fwd(tmpOut, xi);
+    // Store the result in the columns of the STFT matrix
+    stft.col(iCol) = tmpOut;
   }
-  return X;
+  return stft;
 }
+// template <typename T>
+// std::vector<std::vector<std::complex<double>>> stft(std::vector<T> x,
+//                                                     std::vector<double>
+//                                                     window, int nfft, int
+//                                                     noverlap) {
+//   // Hop size
+//   auto hopSize = window.size() - noverlap;
+//   // Number of columns in the STFT matrix
+//   auto k = (int)floor((x.size() - noverlap) / hopSize);
+//   // STFT matrix
+//   auto X = std::vector<std::vector<std::complex<double>>>(
+//       k, std::vector<std::complex<double>>(nfft));
+//   // One window length of time series data
+//   auto xi = std::vector<T>(window.size());
+//   for (auto iStart = 0, iVec = 0; iStart < x.size() - window.size();
+//        iStart += hopSize, iVec++) {
+//     // Apply the window
+//     std::transform(x.begin() + iStart, x.begin() + iStart + window.size(),
+//                    window.begin(), xi.begin(), std::multiplies<T>());
+//     // Compute the FFT
+//     X[iVec] = fft(xi);
+//   }
+//   return X;
+// }
 
 /**
  * @brief Plot the spectrogram (frequency vs. time) of a signal
