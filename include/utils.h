dc76a1d76ee0fad2d71014e7d1b35889f7159d29
shane <shaneflandermeyer@gmail.com>
2021-12-02 15:45:11 -0600

    Added eigen conversion functions

--- include/utils.h
+++ include/utils.h
@@ -3,10 +3,42 @@
 
 #include <fftw3.h>
 
+#include <Eigen/Dense>
 #include <complex>
 #include <vector>
 
-// TODO: These are currently passed by value, but moving data might be expensive
+/**
+ * @brief Convert a vector (or vector of vectors) to an eigen matrix object
+ *
+ * @tparam Scalar Data type of the matrix elements
+ * @tparam Container Type of container storing the data
+ * @param vectors Data to be converted
+ * @return Eigen::Matrix<Scalar, -1, -1> Eigen matrix of the data
+ */
+template <typename Scalar, typename Container>
+inline static Eigen::Matrix<Scalar, -1, -1> toEigen(const Container &vectors) {
+  Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic> M(
+      vectors.size(), vectors.front().size());
+  for (size_t i = 0; i < vectors.size(); i++)
+    for (size_t j = 0; j < vectors.front().size(); j++) M(i, j) = vectors[i][j];
+  return M;
+}
+
+/**
+ * @brief Convert an eigen matrix object to a vector (or vector of vectors)
+ *
+ * @tparam Scalar Data type of the matrix elements
+ * @tparam Matrix Matrix type to convert
+ * @param M Input eigen matrix
+ * @return std::vector< std::vector<Scalar>> Output vector
+ */
+template <typename Scalar, typename Matrix>
+inline static std::vector<std::vector<Scalar>> fromEigen(const Matrix &M) {
+  std::vector<std::vector<Scalar>> m(M.rows(), std::vector<Scalar>(M.cols()));
+  for (size_t i = 0; i < m.size(); i++)
+    for (size_t j = 0; j < m.front().size(); j++) m[i][j] = M(i, j);
+  return m;
+}
 
 /**
  * @brief Matlab-like syntax for computing a complex forward FFT with FFTW3
