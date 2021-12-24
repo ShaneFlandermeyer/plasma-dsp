1cdf7259f944c9e4f5f594c7a8e410f91ca5d250
ShaneFlandermeyer <shaneflandermeyer@gmail.com>
2021-12-24 10:10:37 -0600

    Added FMCW waveform class

---
+++ include/plasma-dsp/fmcwwaveform.h
@@ -0,0 +1,10 @@
+#ifndef B50A8067_E867_4FD6_AE82_CDA1990F0971
+#define B50A8067_E867_4FD6_AE82_CDA1990F0971
+
+#include "waveform.h"
+
+namespace plasma {
+
+class FMCWWaveform : public Waveform {};
+}  // namespace plasma
+#endif /* B50A8067_E867_4FD6_AE82_CDA1990F0971 */
---
+++ lib/fmcwwaveform.cc
@@ -0,0 +1,5 @@
+#include "fmcwwaveform.h"
+
+namespace plasma {
+  
+}
