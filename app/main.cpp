381d0302045aeabf44ef6d13d185144f039100e9
shane <shaneflandermeyer@gmail.com>
2021-12-27 11:42:22 -0600

    Added 2D matrix class

--- app/main.cpp
+++ app/main.cpp
@@ -12,25 +12,20 @@
 #include "vector-utils.h"
 #include "signal-processing.h"
 #include "window.h"
+#include "matrix2d.h"
 
 using namespace matplot;
 using namespace plasma;
 int main() {
-  auto bandwidth = 300;
-  auto pulse_width = 2;
-  auto samp_rate = 1/0.001;
-  std::vector<double> prf = {10e3};
-  auto wave = LinearFMWaveform(bandwidth, pulse_width, prf, samp_rate);
-  wave.freq_offset(150);
-  auto x = wave.waveform();
-  // auto win = hamming(256);
-  auto spec = transpose(spectrogram(x,hamming(256),256,250));
-  spec = db(spec);
-  image(0,pulse_width,-samp_rate/2,samp_rate/2,spec,true);
-  gca()->y_axis().reverse(false);
-  show();
-  // colorbar();
-  // xlabel("Time (s)");
+  auto bandwidth = 10e6;
+  auto pulse_width = 10e-6;
+  auto samp_rate = 20e6;
+  std::vector<double> prf = {1e3};
+  auto lfm = LinearFMWaveform(bandwidth, pulse_width, prf, samp_rate);
+  auto wave = lfm.waveform();
+
+  // auto mat = 
+
 
   return 0;
 }
---
+++ include/plasma-dsp/matrix2d.h
@@ -0,0 +1,120 @@
+#ifndef D7980583_8C42_4289_9C1D_9EC6487DFB3E
+#define D7980583_8C42_4289_9C1D_9EC6487DFB3E
+template <typename T>
+/**
+ * @brief Class representing a 2-dimensional matrix
+ *
+ * This class is primarly intended to provide an interface between STL vectors
+ * and Eigen matrices. The data is stored in row-major order.
+ *
+ */
+class Matrix2D {
+ public:
+  /**
+   * @brief Construct a new Matrix 2 D object
+   *
+   */
+  Matrix2D();
+  /**
+   * @brief Construct a new Matrix 2 D object
+   *
+   * @param num_rows Number of rows
+   * @param num_cols Number of columns
+   */
+  Matrix2D(unsigned int num_rows, unsigned int num_cols);
+  /**
+   * @brief Construct a new Matrix 2 D object
+   *
+   * @param num_rows Number of rows
+   * @param num_cols Number of columns
+   * @param values Vector of values
+   */
+  Matrix2D(unsigned int num_rows, unsigned int num_cols,
+           const std::vector<T>& values);
+
+  /**
+   * @brief Return the element at the given (zero-indexed) row and column
+   * 
+   * @param row Row index
+   * @param col Column index
+   * @return T& Element
+   */
+  T& operator()(unsigned int row, unsigned int col);
+
+  /**
+   * @brief Return a pointer to the matrix data
+   * 
+   * @return auto Pointer to the matrix data vector
+   */
+  auto data() const { return &d_data[0]; };
+
+  /**
+   * @brief Return the number of elements in the matrix
+   * 
+   * @return auto Number of elements
+   */
+  auto numel() const { return &d_data.size(); };
+
+  /**
+   * @brief Return the number of rows in the matrix
+   * 
+   * @return auto 
+   */
+  auto rows() const { return d_num_rows; };
+
+  /**
+   * @brief Return the number of columns in the matrix
+   * 
+   * @return auto 
+   */
+  auto cols() const { return d_num_cols; };
+
+ protected:
+  /**
+   * @brief Data vector
+   *
+   */
+  std::vector<T> d_data;
+  /**
+   * @brief Number of rows
+   *
+   */
+  unsigned int d_num_rows;
+  /**
+   * @brief Number of columns
+   *
+   */
+  unsigned int d_num_cols;
+};
+
+template <typename T>
+Matrix2D<T>::Matrix2D() {
+  d_num_rows = 0;
+  d_num_cols = 0;
+  d_data = std::vector<T>(d_num_rows * d_num_cols);
+}
+
+template <typename T>
+Matrix2D<T>::Matrix2D(unsigned int num_rows, unsigned int num_cols) {
+  d_num_rows = num_rows;
+  d_num_cols = num_cols;
+  d_data = std::vector<T>(d_num_rows * d_num_cols);
+}
+
+template <typename T>
+Matrix2D<T>::Matrix2D(unsigned int num_rows, unsigned int num_cols,
+                      const std::vector<T>& values) {
+  d_num_rows = num_rows;
+  d_num_cols = num_cols;
+  d_data = values;
+}
+
+template <typename T>
+T& Matrix2D<T>::operator()(unsigned int row, unsigned int col) {
+  if (row >= d_num_rows || col >= d_num_cols) {
+    throw std::out_of_range("Matrix2D: index out of range");
+  }
+  return data[row * d_num_cols + col];
+}
+
+#endif /* D7980583_8C42_4289_9C1D_9EC6487DFB3E */
