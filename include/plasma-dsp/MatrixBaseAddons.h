1834aeec7b9d98c632fb5933887e21f81de20ec5
ShaneFlandermeyer <shaneflandermeyer@gmail.com>
2022-01-01 10:18:51 -0600

    Removed matrix2d class and updated MatrixBaseAddons

--- include/plasma-dsp/MatrixBaseAddons.h
+++ include/plasma-dsp/MatrixBaseAddons.h
@@ -1,33 +1,20 @@
-#ifndef C63AD55F_46BC_4875_BB0D_2D632194F831
-#define C63AD55F_46BC_4875_BB0D_2D632194F831
-inline Scalar at(uint i, uint j) const { return this->operator()(i,j); }
-inline Scalar& at(uint i, uint j) { return this->operator()(i,j); }
-inline Scalar at(uint i) const { return this->operator[](i); }
-inline Scalar& at(uint i) { return this->operator[](i); }
-inline RealScalar squaredLength() const { return squaredNorm(); }
-inline RealScalar length() const { return norm(); }
-inline RealScalar invLength(void) const { return fast_inv_sqrt(squaredNorm()); }
-template<typename OtherDerived>
-inline Scalar squaredDistanceTo(const MatrixBase<OtherDerived>& other) const
-{ return (derived() - other.derived()).squaredNorm(); }
-template<typename OtherDerived>
-inline RealScalar distanceTo(const MatrixBase<OtherDerived>& other) const
-{ return internal::sqrt(derived().squaredDistanceTo(other)); }
-inline void scaleTo(RealScalar l) { RealScalar vl = norm(); if (vl>1e-9) derived() *= (l/vl); }
-inline Transpose<Derived> transposed() {return this->transpose();}
-inline const Transpose<Derived> transposed() const {return this->transpose();}
-inline uint minComponentId(void) const  { int i; this->minCoeff(&i); return i; }
-inline uint maxComponentId(void) const  { int i; this->maxCoeff(&i); return i; }
-template<typename OtherDerived>
-void makeFloor(const MatrixBase<OtherDerived>& other) { derived() = derived().cwiseMin(other.derived()); }
-template<typename OtherDerived>
-void makeCeil(const MatrixBase<OtherDerived>& other) { derived() = derived().cwiseMax(other.derived()); }
-const CwiseUnaryOp<internal::scalar_add_op<Scalar>, Derived>
-operator+(const Scalar& scalar) const
-{ return CwiseUnaryOp<internal::scalar_add_op<Scalar>, Derived>(derived(), internal::scalar_add_op<Scalar>(scalar)); }
-friend const CwiseUnaryOp<internal::scalar_add_op<Scalar>, Derived>
-operator+(const Scalar& scalar, const MatrixBase<Derived>& mat)
-{ return CwiseUnaryOp<internal::scalar_add_op<Scalar>, Derived>(mat.derived(), internal::scalar_add_op<Scalar>(scalar)); }
 
-
-#endif /* C63AD55F_46BC_4875_BB0D_2D632194F831 */
+/**
+ * @brief Convert an eigen matrix into a vector of std vectors
+ *
+ * The only reason one generally wants to do this is to plot something with
+ * matplot++
+ *
+ * @return std::vector<std::vector<Scalar>> 2d vector representing the eigen
+ * matrix
+ */
+std::vector<std::vector<Scalar>> vector2d() {
+  std::vector<std::vector<Scalar>> vec(this->rows(),
+                                       std::vector<Scalar>(this->cols()));
+  for (uint i = 0; i < this->rows(); i++) {
+    for (uint j = 0; j < this->cols(); j++) {
+      vec[i][j] = this->operator()(i, j);
+    }
+  }
+  return vec;
+}
