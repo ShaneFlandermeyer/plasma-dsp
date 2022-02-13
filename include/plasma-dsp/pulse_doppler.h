a19149b9c3a74fb8971736eea1ebedb7dcd237c6
ShaneFlandermeyer <shaneflandermeyer@gmail.com>
2022-02-12 21:18:05 -0600

    Implemented delay function for both real and complex inputs

--- include/plasma-dsp/pulse_doppler.h
+++ include/plasma-dsp/pulse_doppler.h
@@ -11,13 +11,12 @@ namespace plasma {
 
 /**
  * @brief Shift zero-frequency component to center of spectrum
- * 
+ *
  * @tparam T Input element type
  * @param data Input data
  * @param count Input data size
  */
-template <typename T> 
-inline void fftshift(T *data, const size_t count) {
+template <typename T> inline void fftshift(T *data, const size_t count) {
   int center = (int)floor((float)count / 2);
   if (count % 2 != 0) {
     center++;
@@ -28,21 +27,53 @@ inline void fftshift(T *data, const size_t count) {
 
 /**
  * @brief Inverse zero-frequency shift
- * 
+ *
  * @tparam T Input element type
  * @param data Input data
  * @param count Input data size
  */
-template <typename T>
-inline void ifftshift(T *data, const size_t count) {
+template <typename T> inline void ifftshift(T *data, const size_t count) {
   int center = (int)floor((float)count / 2);
   // odd: 01 234 changes to 234 01
   std::rotate(data, data + center, data + count);
 }
 
 /**
- * @brief Use Fourier interpolation to delay an input signal by an arbitrary
- * amount of time (i.e., a non-integer number of samples).
+ * @brief Delay a complex input signal by an arbitrary amount of time (i.e., a
+ * non-integer number of samples) using Fourier interpolation
+ *
+ * TODO: For now, the input signal must be a Vector object
+ *
+ * @tparam T Data type of the elements in the signal
+ * @param x Input signal vector
+ * @param t Time delay (s)
+ * @param nfft Number of points in the FFT
+ * @param fs Sample rate (samples/sec)
+ * @return Eigen::VectorX<T>
+ */
+template <typename T>
+Eigen::VectorX<std::complex<T>> delay(const Eigen::VectorX<std::complex<T>> &x,
+                                      double t, size_t nfft, double fs) {
+  Eigen::FFT<double> fft;
+  // Compute the shifted FFT of the input signal
+  Eigen::VectorXcd x_fft = x.template cast<std::complex<double>>();
+  x_fft.conservativeResize(nfft);
+  fft.fwd(x_fft, x_fft);
+  fftshift(x_fft.data(), x_fft.size());
+  // Apply the delay as a phase shift in the frequency domain
+  Eigen::ArrayXd F =
+      Eigen::ArrayXd::LinSpaced(nfft, 0.0, nfft - 1) * (fs / nfft) - (fs / 2);
+  x_fft = x_fft.array() * exp(-Im * 2.0 * M_PI * F * t);
+  // IFFT back to the time domain
+  Eigen::VectorXcd y(nfft, 1);
+  ifftshift(x_fft.data(), x_fft.size());
+  fft.inv(y, x_fft);
+  return y.cast<std::complex<T>>();
+}
+
+/**
+ * @brief Delay a real input signal by an arbitrary amount of time (i.e., a
+ * non-integer number of samples) using Fourier interpolation.
  *
  * TODO: For now, the input signal must be a column vector
  *
@@ -51,25 +82,14 @@ inline void ifftshift(T *data, const size_t count) {
  * @param t Time delay (s)
  * @param nfft Number of points in the FFT
  * @param fs Sample rate (samples/sec)
- * @return Eigen::ArrayX<T>
+ * @return Eigen::VectorX<T>
  */
 template <typename T>
-Eigen::MatrixX<T> delay(Eigen::MatrixX<T> x, double t, size_t nfft, double fs) {
-  Eigen::FFT<T> fft;
-  // Compute the shifted FFT of the input signal
-  Eigen::MatrixXcd x_fft(nfft, 1);
-  fft.fwd(x_fft.data(), x.data(), nfft);
-  fftshift(x_fft.data(), x_fft.size());
-  // Apply the delay as a phase shift in the frequency domain
-  Eigen::ArrayXd F =
-      Eigen::ArrayXd::LinSpaced(nfft, 0.0, nfft - 1) * (fs / nfft) - (fs / 2);
-  x_fft = x_fft.array() * exp(-Im * 2.0 * M_PI * F * t);
-  // IFFT back to the time domain
-  Eigen::MatrixX<T> y(nfft, 1);
-  ifftshift(x_fft.data(), x_fft.size());
-  fft.inv(y.data(), x_fft.data(), x_fft.size());
-  std::cout << y << std::endl;
-  return y;
+Eigen::VectorX<T> delay(const Eigen::VectorX<T> &x, double t, size_t nfft,
+                        double fs) {
+  Eigen::VectorX<std::complex<T>> x_fft = x.template cast<std::complex<T>>();
+  Eigen::VectorX<std::complex<T>> y = delay(x_fft, t, nfft, fs);
+  return y.real().template cast<T>();
 }
 
 } // namespace plasma
