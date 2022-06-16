752105aa818ca6ec7551a89383573f5573d8e5f1
ShaneFlandermeyer <shaneflandermeyer@gmail.com>
2022-06-16 09:25:11 -0500

    Added basic FFT class

--- include/plasma_dsp/fft.h
+++ include/plasma_dsp/fft.h
@@ -3,19 +3,25 @@
 
 #include <complex>
 #include <iostream>
+#include <Eigen/Dense>
 
 namespace plasma {
 
 template <class T> class FFT {
 private:
-  int d_num_threads;
+  size_t d_num_threads;
+  size_t d_size;
+  Eigen::ArrayX<T> d_input;
+  Eigen::ArrayX<T> d_output;
   void *d_plan;
 
 public:
-  FFT();
   FFT(size_t fft_size, size_t num_threads = 1);
   ~FFT();
   void execute();
+
+  T *input() { return d_input.data(); }
+  T *output() { return d_output.data(); }
 };
 
 } // namespace plasma
--- lib/fft.cc
+++ lib/fft.cc
@@ -1,19 +1,30 @@
 #include "fft.h"
+#include <fftw3.h>
 
 namespace plasma {
-template <class T> FFT<T>::FFT() { std::cout << "TODO: Default constructor" << std::endl; }
-
 template <class T> FFT<T>::~FFT() {
-  std::cout << "TODO: Destructor" << std::endl;
-}
-
-template <class T> FFT<T>::FFT(size_t fft_size, size_t num_threads) {
-  std::cout << "TODO: Constructor" << std::endl;
+  fftwf_destroy_plan((fftwf_plan)d_plan);
+  fftw_cleanup_threads();
 }
 
 template <class T>
-void FFT<T>::execute() {
-std::cout << "TODO: Execute FFT" << std::endl;
+FFT<T>::FFT(size_t fft_size, size_t num_threads)
+    : d_size(fft_size), d_num_threads(num_threads), d_input(fft_size),
+      d_output(fft_size) {
+  // Handle threads
+  if (num_threads > 1) {
+    fftwf_init_threads();
+    fftwf_plan_with_nthreads(num_threads);
+  }
+
+  // Create the plan
+  d_plan = fftwf_plan_dft_1d(fft_size,
+                             reinterpret_cast<fftwf_complex *>(d_input.data()),
+                             reinterpret_cast<fftwf_complex *>(d_output.data()),
+                             FFTW_FORWARD, FFTW_MEASURE);
+}
+
+template <class T> void FFT<T>::execute() { fftwf_execute((fftwf_plan)d_plan); 
 }
 
 template class FFT<std::complex<float>>;
