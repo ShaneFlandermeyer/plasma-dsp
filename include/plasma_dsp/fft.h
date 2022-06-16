1fa051de4a5cfc05e6e3e9b1364d45825d4cf728
ShaneFlandermeyer <shaneflandermeyer@gmail.com>
2022-06-16 16:29:56 -0500

    Added object for computing FFTs on doubles

--- include/plasma_dsp/fft.h
+++ include/plasma_dsp/fft.h
@@ -17,10 +17,18 @@ template <> struct fft_input<float, false> {
   typedef std::complex<float> type;
 };
 
+template <> struct fft_input<double, false> {
+  typedef std::complex<double> type;
+};
+
 template <class T, bool forward> struct fft_output { typedef T type; };
 
 template <> struct fft_output<float, true> {
   typedef std::complex<float> type;
+};
+
+template <> struct fft_output<double, true> {
+  typedef std::complex<double> type;
 };
 
 /**
@@ -101,13 +109,7 @@ public:
    *
    * @return fft_output<T, forward>::type*
    */
-  fft_output<T, forward>::type *output() {
-    typename fft_output<T, forward>::type *out =
-        new typename fft_output<T, forward>::type[d_size];
-    memcpy(out, d_output.data(),
-           sizeof(typename fft_output<T, forward>::type) * d_size);
-    return d_output.data();
-  }
+  fft_output<T, forward>::type *output();
 
   int input_length() const { return d_input.size(); }
   int output_length() const { return d_output.size(); }
@@ -118,42 +120,103 @@ public:
    */
   void execute();
 
-  fft_output<T, forward>::type *execute(typename fft_input<T, forward>::type *input) {
-    memcpy(d_input.data(), input,
-           sizeof(typename fft_input<T, forward>::type) * d_size);
-    execute();
-    return output();
-  }
+  fft_output<T, forward>::type *
+  execute(typename fft_input<T, forward>::type *input);
 };
 
-// /**
-//  * @brief Template class for performing FFTs on floating point data.
-//  *
-//  * This implementation is basically a copy-paste version of the
-//  implementation
-//  * in gr-fft
-//  *
-//  * @tparam T Input data type (float or std::complex<float>)
-//  */
-// template <class T, bool forward> class FFT {
-// private:
-//   size_t d_num_threads;
-//   size_t d_size;
-//   Eigen::ArrayX<T> d_input;
-//   Eigen::ArrayX<T> d_output;
-//   void *d_plan;
+/**
+ * @brief Class implementing a 1D FFT/IFFT for doubles.
+ *
+ * This is basically identical to the fft class in gnuradio's gr-fft, but
+ * without the gnuradio-specific stuff.
+ *
+ * @tparam T The input data type if a forward FFT is performed, and the output
+ * type if a reverse FFT is performed
+ * @tparam forward If true, the object computes a forward FFT. If false, it
+ * computes an IFFT
+ */
 
-// public:
-//   FFT(size_t fft_size, size_t num_threads = 1);
-//   ~FFT();
-//   void execute();
+template <bool forward> class FFT<double, forward> {
+  /**
+   * @brief Number of threads to use in the computation
+   *
+   */
+  size_t d_num_threads;
 
-//   T *input() { return d_input.data(); }
-//   void input(T *input) { memcpy(d_input.data(), input, sizeof(T) * d_size);
-//   } T *output() { return d_output.data(); } void output(T *output) {
-//     memcpy(output, d_output.data(), sizeof(T) * d_size);
-//   }
-// };
+  /**
+   * @brief FFT size
+   *
+   */
+  size_t d_size;
+
+  /**
+   * @brief Input array
+   *
+   */
+  Eigen::ArrayX<typename fft_input<double, forward>::type> d_input;
+
+  /**
+   * @brief Output array
+   *
+   */
+  Eigen::ArrayX<typename fft_output<double, forward>::type> d_output;
+
+  /**
+   * @brief FFTW plan
+   *
+   */
+  void *d_plan;
+
+  /**
+   * @brief Create a plan based on the size, data type, and FFT direction
+   *
+   * @param fft_size
+   */
+  void initialize_plan(size_t fft_size);
+
+public:
+  FFT(size_t fft_size, size_t nthreads = 1);
+  virtual ~FFT();
+
+  /**
+   * @brief Get a pointer to the input buffer
+   *
+   * @return fft_input<T, forward>::type*
+   */
+  typename fft_input<typename fft_input<double, forward>::type, forward>::type *
+  input() {
+    return d_input.data();
+  }
+
+  /**
+   * @brief Set the data in the input array
+   *
+   * @param input
+   */
+  void input(typename fft_input<double, forward>::type *input) {
+    memcpy(d_input.data(), input,
+           sizeof(typename fft_input<double, forward>::type) * d_size);
+  }
+
+  /**
+   * @brief Return a pointer to a COPY of the output array
+   *
+   * @return fft_output<T, forward>::type*
+   */
+  fft_output<double, forward>::type *output();
+
+  int input_length() const { return d_input.size(); }
+  int output_length() const { return d_output.size(); }
+
+  /*!
+   * compute FFT. The input comes from input, the output is placed in
+   * output.
+   */
+  void execute();
+
+  fft_output<double, forward>::type *
+  execute(typename fft_input<double, forward>::type *input);
+};
 
 } // namespace plasma
 
