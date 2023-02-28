8970803adec67f092770697756e4ae87b22a467b
ShaneFlandermeyer <shaneflandermeyer@gmail.com>
2023-02-28 06:31:03 -0600

    Implemented the RLD

--- python/plasma_dsp/deconv.py
+++ python/plasma_dsp/deconv.py
@@ -1,14 +1,20 @@
+import time
 import jax.numpy as jnp
+import jax.scipy as jsp
 from common import delay, doppler_shift
 from linear_fm import lfm
 import matplotlib.pyplot as plt
 from pulse_doppler import filter_lti, process_doppler
 
 
-def richardson_lucy(range_doppler: jnp.ndarray,
-                    psf: jnp.ndarray,
+def richardson_lucy(RD: jnp.ndarray,
+                    P: jnp.ndarray,
                     ) -> jnp.ndarray:
-  pass
+  J = RD
+  for i in range(1):
+    frac = RD / jnp.fft.ifft2(jnp.fft.fft2(J) * jnp.fft.fft2(P, J.shape))
+    J = J * (jnp.fft.ifft2(jnp.fft.fft2(frac) * jnp.fft.fft2(P, J.shape)))
+  return J
 
 
 if __name__ == '__main__':
@@ -27,7 +33,7 @@ if __name__ == '__main__':
            samp_rate=fs,
            start_freq=-bw / 4)
 
-  # TODO: Simulate the pulses with a delay and/or doppler shift
+  # Simulate the pulses with a delay and/or doppler shift
   # Logic: 8 pulses of x1 are sent, followed by 2 pulses of x2
   prf = 10e3
   np = 128
@@ -47,18 +53,27 @@ if __name__ == '__main__':
 
   # Doppler processing
   dynamic_range_db = 70
+  
   rdm = process_doppler(range_slow_time)
-  rdm = rdm.at[jnp.abs(rdm) < 10**(-dynamic_range_db/20)
-               ].set(10**(-dynamic_range_db/20))
-  rdm = rdm / jnp.max(rdm)
+  
+  
+  # For testing, use the same RDM for both arguments in the RL-deconvolution
+  start = time.time()
+  richardson_lucy(rdm, rdm)
+  print(f'Processing time: {time.time() - start:.2f} s')
+  
+  
+  # rdm = rdm.at[jnp.abs(rdm) < 10**(-dynamic_range_db/20)
+  #              ].set(10**(-dynamic_range_db/20))
+  # rdm = rdm / jnp.max(rdm)
 
   # freq_pulse = jnp.fft.fftshift(jnp.fft.fft(fast_slow_time, axis=0), axes=0)
   # Faxis = jnp.fft.fftshift(jnp.fft.fftfreq(freq_pulse.shape[0], d=1/fs))
   # plt.imshow(abs(freq_pulse), aspect='auto', origin='lower',
   #            extent=(0, np, Faxis[0] / 1e6, Faxis[-1] / 1e6))
   # plt.plot(20*jnp.log10(abs(range_slow_time)))
-  plt.imshow(20*jnp.log10(jnp.abs(rdm)), aspect='auto',
-             origin='lower', cmap='plasma')
-  plt.colorbar()
-  plt.clim([-dynamic_range_db, 0])
-  plt.show()
+  # plt.imshow(20*jnp.log10(jnp.abs(rdm)), aspect='auto',
+  #            origin='lower', cmap='plasma')
+  # plt.colorbar()
+  # plt.clim([-dynamic_range_db, 0])
+  # plt.show()
